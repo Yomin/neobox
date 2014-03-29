6aebb3c48fecd6b5e828cd32539d26ff2067a9c6
Martin Roedel <lordyomin@hivegate.net>
2014-03-29 19:51:22 +0100

    console: remove daemon mode, some cleanup

--- console/console.c
+++ console/console.c
@@ -82,27 +82,23 @@ int handler(struct tkbio_return ret, void *state)
 
 void usage(char *name)
 {
-    printf("Usage: %s [-s] [-d] <tty>\n", name);
+    printf("Usage: %s [-s] <tty>\n", name);
 }
 
 int main(int argc, char* argv[])
 {
-    int opt;
+    int opt, fd, err, ret;
     int show = 0;
-    int daemon = 0;
     char *tty;
     
     struct tkbio_config config = tkbio_config_default(&argc, argv);
     
-    while((opt = getopt(argc, argv, "sd")) != -1)
+    while((opt = getopt(argc, argv, "s")) != -1)
     {
         switch(opt)
         {
         case 's':
             show = 1;
-            break;
-        case 'd':
-            daemon = 1;
             break;
         default:
             usage(argv[0]);
@@ -118,62 +114,24 @@ int main(int argc, char* argv[])
     
     tty = argv[optind];
     
-    if(daemon)
-    {
-        int tmp = fork();
-        if(tmp < 0)
-        {
-            perror("Failed to fork process");
-            return 1;
-        }
-        if(tmp > 0)
-        {
-            return 0;
-        }
-        
-        setsid();
-        
-        close(0); close(1); close(2);
-        tmp = open("/dev/null", O_RDWR); dup(tmp); dup(tmp);
-        
-        if(mkdir("/var/" NAME, 0755) == -1 && errno != EEXIST)
-        {
-            perror("Failed to create working dir");
-            return 2;
-        }
-        chdir("/var/" NAME);
-        
-        if((tmp = open("pid", O_WRONLY|O_CREAT, 755)) < 0)
-        {
-            perror("Failed to open pidfile");
-            return 3;
-        }
-        
-        pid_t pid = getpid();
-        char buf[10];
-        int count = sprintf(buf, "%i", pid);
-        write(tmp, &buf, count*sizeof(char));
-        close(tmp);
-    }
-    
-    int fd;
     if(!show)
         config.options |= TKBIO_OPTION_NO_INITIAL_PRINT;
     
-    if((fd = tkbio_init_custom(config)) < 0)
-        return fd;
+    if((ret = tkbio_init_custom(config)) < 0)
+        return ret;
     
     if((fd = open(tty, O_RDONLY)) < 0)
     {
-        int e = errno;
+        err = errno;
         perror("Failed to open tty");
         tkbio_finish();
-        return e;
+        return err;
     }
     
-    int ret = tkbio_run(handler, &fd);
+    ret = tkbio_run(handler, &fd);
     tkbio_finish();
     close(fd);
+    
     return ret;
 }
 
